d97be85bf867d628f2034abdbae10ab031f8776b
BeepBoop <markhamonca@codeninjas.com>
2024-07-28 13:49:39 -0400

    52

--- Shawn asteroids/Assets/GameManager.cs
+++ Shawn asteroids/Assets/GameManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject rockPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 randomPosition = new Vector3(Random.Range(-8, 8), Random.Range(-8,8), 0f);
+        Instantiate(rockPrefab, Vector3.zero, Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
--- Shawn asteroids/Assets/ship.cs
+++ Shawn asteroids/Assets/ship.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class ship : MonoBehaviour
 {
+    public float lookSpeed;
+    public float maxSpeed;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public GameObject bulletPrefab;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * lookSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * lookSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 velocity = rb.velocity;
+            velocity = velocity + transform.right  * Time.deltaTime * moveSpeed;
+            Vector3.ClampMagnitude(velocity, maxSpeed);
+            rb.velocity = velocity;
+        }
+
+
     }
 }
