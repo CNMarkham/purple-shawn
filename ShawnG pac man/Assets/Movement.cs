fa1cb17dfadbf95db6385e83eb7296c6ab4113d8
Hayden <markhamonca@codeninjas.com>
2024-08-18 13:50:16 -0400

    21

---
+++ ShawnG pac man/Assets/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movement : MonoBehaviour
+{
+    public float speed;
+    public Vector2 initalDirection;
+    public LayerMask obstacleLayer;
+
+    protected Rigidbody2D rb;
+    protected Vector2 direction;
+    protected Vector2 nextDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = initalDirection;
+        nextDirection = Vector2.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(position + translation);
+    }
+} 
