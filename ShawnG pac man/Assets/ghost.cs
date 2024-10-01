b0d13280230a3c66a363f725c1c23bf8a516806c
Is <markhamonca@codeninjas.com>
2024-10-01 18:58:49 -0400

    96

--- ShawnG pac man/Assets/ghost.cs
+++ ShawnG pac man/Assets/ghost.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Ghost : Movement
 {
+    GameObject body;
+    GameObject eyes;
+    GameObject blue;
+    GameObject white;
+    bool frightend;
+    bool atHome;
+    float homeDuration;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Node node = collision.GetComponent<Node>();
@@ -11,10 +18,15 @@ public class Ghost : Movement
         if (node != null)
         {
             int index = Random.Range(0, node.availableDirections.Count);
+
+            if (node.availableDirections[index] == -direction)
             {
                 index += 1;
 
-                
+                if (index == node.availableDirections.Count)
+                {
+                    index = 0;
+                }
             }
 
             SetDirection(node.availableDirections[index]);
@@ -26,4 +38,20 @@ public class Ghost : Movement
 
     }
 
+    private void Awake()
+    {
+        body.SetActive(true);
+        eyes.SetActive(true);
+        blue.SetActive(false);
+        white.SetActive(false);
+        frightend = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (atHome && collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            SetDirection(-direction);
+        }
+    }
 }
