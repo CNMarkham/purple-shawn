bf1a5d931bef35ff1ae548b4457db1e337422ea7
reneee <alec.c2027@gmail.com>
2025-02-22 11:11:02 -0500

    tilemap

--- super mario shawn purple/Assets/Scrites/Enemymovement.cs
+++ super mario shawn purple/Assets/Scrites/Enemymovement.cs
@@ -7,6 +7,13 @@ public class Enemymovement : MonoBehaviour
     public float speed = 2f;
         public Vector2 direction = Vector2.left;
     private Rigidbody2D rb;
+<<<<<<< Updated upstream:super mario shawn purple/Assets/Scrites/Enemymovement.cs
+=======
+    public float moveSpeed;
+
+    private RaycastHit2D hit;
+    public float jumpForce;
+>>>>>>> Stashed changes:super mario shawn purple/Assets/PlayerMovement.cs
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +26,25 @@ public class Enemymovement : MonoBehaviour
         
     }
 
+<<<<<<< Updated upstream:super mario shawn purple/Assets/Scrites/Enemymovement.cs
     private void OnBecameVisible()
     {
         rb.velocity = direction * speed;
+=======
+        rb.AddForce(Vector2.right * horizontal * moveSpeed * Time.deltaTime);
+
+        Jump();
+    }
+    private void Jump()
+    {
+        hit = Physics2D.CircleCast(rb.position, 0.25f, Vector2.down, 0.375f, LayerMask.GetMask("Default"));
+        if (hit.collider != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 velocity = rb.velocity;
+            velocity.y = jumpForce;
+            rb.velocity = velocity;
+        }
+
+>>>>>>> Stashed changes:super mario shawn purple/Assets/PlayerMovement.cs
     }
 }
